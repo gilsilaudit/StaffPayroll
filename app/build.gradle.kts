9be662a8ec103943f9da9a6ffeb6abf460964999
gilsilaudit <gilsilaudit@gmail.com>
2026-08-29 23:13:40 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -11,8 +10,10 @@ android {
 
     defaultConfig {
         applicationId = "com.nitin.staffpayroll"
+
         minSdk = 24
         targetSdk = 37
+
         versionCode = 1
         versionName = "0.1.0"
 
@@ -41,10 +42,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlin {
-        jvmToolchain(17)
-    }
-
     buildFeatures {
         compose = true
     }
@@ -57,25 +54,47 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
+
+    val composeBom =
+        platform("androidx.compose:compose-bom:2026.08.00")
 
     implementation(composeBom)
+
     androidTestImplementation(composeBom)
 
     implementation("androidx.core:core-ktx:1.19.0")
+
     implementation("androidx.activity:activity-compose:1.13.0")
 
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
 
     testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(
+        "androidx.test.ext:junit:1.3.0"
+    )
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation(
+        "androidx.test.espresso:espresso-core:3.7.0"
+    )
+
+    androidTestImplementation(
+        "androidx.compose.ui:ui-test-junit4"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-test-manifest"
+    )
 }
