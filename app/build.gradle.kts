e912d466e705551a2ab6226892f17dc9bc5674a1
gilsilaudit <gilsilaudit@gmail.com>
2026-09-03 22:15:22 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.speqta.staffpayroll"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.3.3"
+        versionCode = 14
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
